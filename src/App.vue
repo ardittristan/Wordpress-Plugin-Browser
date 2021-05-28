5c3e2520a7f663e8f3fe4800b448c157958482d1
ardittristan <42787508+ardittristan@users.noreply.github.com>
2021-05-28 16:57:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <header>Wordpress Plugin Browser</header>
+      <v-spacer />
+      <v-text-field
+        v-model="search"
+        solo
+        hide-details
+        dense
+        clearable
+        placeholder="Search Plugin"
+        @keyup="onTyping()"
+        @click:clear="onTyping()"
+      >
+        <template v-slot:label>
+          Search Plugin <v-icon dense>mdi-magnify</v-icon>
+        </template>
+      </v-text-field>
+      <v-spacer />
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      pagenav
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  import { Unquery, replaceLocationURL } from "unquery";
+  import axios from "axios";
+
+  export default {
+    name: "App",
+
+    components: {},
+
+    data: () => ({
+      search: "",
+      page: 1,
+      /** @type {import('./assets/types').WordpressPlugin[]} */
+      plugins: [],
+      pages: 0
+    }),
+
+    methods: {
+      onTyping() {
+        clearTimeout(this.timeout);
+        var self = this;
+        this.timeout = setTimeout(function () {
+          self.searchFunction();
+        }, 250);
+      },
+      searchFunction() {
+        let searchParam = Unquery(window.location.search, {
+          search: Unquery.string(),
+          page: Unquery.number(),
+        });
+        searchParam.search = this.search;
+        searchParam.page = 1;
+        replaceLocationURL(searchParam);
+        this.getPlugins(this.search, this.page);
+      },
+      /**
+       * @param search {String}
+       * @param page {Number}
+       */
+      async getPlugins(search, page) {
+        /** @type {import('./assets/types').ApiResponse} */
+        let response = (await axios.get(`https://api.wordpress.org/plugins/info/1.2/?action=query_plugins&request%5Bpage%5D=${page}&request%5Bsearch%5D=${encodeURI(search || "")}`)).data;
+        this.pages = response.info.pages;
+        this.plugins = response.plugins;
+      },
+    },
+
+    mounted() {
+      let searchParam = Unquery(window.location.search, {
+        search: Unquery.string(),
+        page: Unquery.number(),
+      });
+
+      if (searchParam.search) this.search = searchParam.search;
+      if (searchParam.page) this.page = searchParam.page;
+
+      this.getPlugins(this.search, this.page);
+    },
+  };
+</script>
---
+++ src/assets/types.d.ts
@@ -0,0 +1,44 @@
+export interface ApiResponse {
+  info: {
+    page: number;
+    pages: number;
+    results: number;
+  };
+  plugins: WordpressPlugin[];
+};
+
+export interface WordpressPlugin {
+  active_installs: number;
+  added: string;
+  author: string;
+  author_profile: string;
+  description: string;
+  donate_link: string;
+  download_link: string;
+  downloaded: number;
+  homepage: string;
+  icons: {
+    "1x": string;
+    "2x"?: string;
+  };
+  last_updated: string;
+  name: string;
+  num_ratings: number;
+  rating: number;
+  ratings: {
+    1: number;
+    2: number;
+    3: number;
+    4: number;
+    5: number;
+  };
+  requires: number;
+  requires_php: number;
+  short_description: string;
+  slug: string;
+  support_threads: number;
+  support_threads_resolved: number;
+  tags: Object;
+  tested: string;
+  version: string;
+};
