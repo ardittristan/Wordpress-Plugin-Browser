bfcd0c2e95aacee0c9f9852a6eed1b4ea6a1329a
ardittristan <ardittristan@gmail.com>
2021-06-01 00:03:43 +0200

    add workflow

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 const AbsoluteModuleMapperPlugin = require("absolute-module-mapper-plugin");
+const settings = require("./settings.json");
 
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  publicPath: process.env.NODE_ENV === "production" ? settings.publicPath : "/",
+
   chainWebpack: (config) => {
     config.resolve.plugin("absolute-module-mapper-plugin").use(AbsoluteModuleMapperPlugin, [
       {
